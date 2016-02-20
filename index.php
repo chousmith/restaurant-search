4e49ed0ea46d5d9eeb818fd09847deafff5e22b9
alex chousmith <alex@ninthlink.com>
2016-02-20 10:50:27 -0800

    adding meta title and description w/ link to github

--- index.php
+++ index.php
@@ -11,7 +11,10 @@
   <title>Tasty Restaurants in the Kearny Mesa area</title>
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width,initial-scale=1">
-
+  
+  <meta propert="og:title" content="Tasty Restaurants in the Kearny Mesa area">
+  <meta propert="og:description" content="https://github.com/chousmith/restaurant-search">
+  
   <!-- Bootstrap : Latest compiled and minified CSS -->
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
